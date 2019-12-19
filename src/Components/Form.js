c584bcfbf8914702a50a5a711a98e120e66fda84
jamalamar <jamal_ab22@hotmail.com>
2019-12-19 15:16:46 -0600

    contact page ready

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,60 +8,60 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
  
  function ContactForm (){
- 	return(
- 	
- 	<div style={style.formContainer}>
-		<Form name="JinxContactForm" method="post" style={style.form}>
+ 	return(	 	
+	 	<div style={style.formContainer}>
 
-			{/*Value set to the same of the Form on Index.html*/}
-		    <input type="hidden" name="form-name" value="JinxContactForm" />
+			<Form name="JinxContactForm" method="post" style={style.form}>
 
-		 <Form.Group controlId="formName&LastName">
-		  <Row>
-		    <Col>
-			  <Form.Label>First Name</Form.Label>
-		      <Form.Control placeholder="Enter first name" name="FirstName" required/>
-		    </Col>
-		    <Col>
-			  <Form.Label>Last Name</Form.Label>
-		      <Form.Control placeholder="Enter last name" name="LastName" required/>
-		    </Col>
-		  </Row>
-		 </Form.Group>
+				{/*Value set to the same of the Form on Index.html*/}
+			    <input type="hidden" name="form-name" value="JinxContactForm" />
 
-		 <Form.Group controlId="formPhone&Country">
-		  <Row>
-		    <Col>
-			  <Form.Label>Country</Form.Label>
-		      <Form.Control placeholder="Enter country" name="Country" required/>
-		    </Col>
-		    <Col>
-			  <Form.Label>Phone Number</Form.Label>
-		      <Form.Control placeholder="Enter phone" name="Phone" required/>
-		    </Col>
-		  </Row>
-		 </Form.Group>						 
+			 <Form.Group controlId="formName&LastName">
+			  <Row>
+			    <Col>
+				  <Form.Label>First Name</Form.Label>
+			      <Form.Control placeholder="Enter first name" name="FirstName" required/>
+			    </Col>
+			    <Col>
+				  <Form.Label>Last Name</Form.Label>
+			      <Form.Control placeholder="Enter last name" name="LastName" required/>
+			    </Col>
+			  </Row>
+			 </Form.Group>
 
-		  <Form.Group controlId="formBasicEmail">
-		    <Form.Label>Email Address</Form.Label>
-		    <Form.Control type="email" placeholder="Enter email" name="Email" required/>
-		    <Form.Text className="text-muted">
-		      We'll never share your email with anyone else.
-		    </Form.Text>
-		  </Form.Group>
+			 <Form.Group controlId="formPhone&Country">
+			  <Row>
+			    <Col>
+				  <Form.Label>Country</Form.Label>
+			      <Form.Control placeholder="Enter country" name="Country" required/>
+			    </Col>
+			    <Col>
+				  <Form.Label>Phone Number</Form.Label>
+			      <Form.Control placeholder="Enter phone" name="Phone" required/>
+			    </Col>
+			  </Row>
+			 </Form.Group>						 
+
+			  <Form.Group controlId="formBasicEmail">
+			    <Form.Label>Email Address</Form.Label>
+			    <Form.Control type="email" placeholder="Enter email" name="Email" required/>
+			    <Form.Text className="text-muted">
+			      We'll never share your email with anyone else.
+			    </Form.Text>
+			  </Form.Group>
 
 
-		  <Form.Group controlId="exampleForm.ControlTextarea1">
-		    <Form.Label>Message</Form.Label>
-		    <Form.Control as="textarea" rows="3" name="Message"/>
-		  </Form.Group>
+			  <Form.Group controlId="exampleForm.ControlTextarea1">
+			    <Form.Label>Message</Form.Label>
+			    <Form.Control as="textarea" rows="3" name="Message"/>
+			  </Form.Group>
 
-		  <Button variant="primary" type="submit">
-		    Submit
-		  </Button>
+			  <Button variant="primary" type="submit">
+			    Submit
+			  </Button>
 
-		</Form>
-	</div>	
+			</Form>
+		</div>
 	)
 }
 
@@ -70,7 +70,7 @@ const style = {
 	formContainer: {
 	  	display: 'flex',
 	  	justifyContent: 'center',
-	  	margin: '4em 0em 9em 0em',
+	  	margin: '4em 0em 2em 0em',
 	 },
 	form: {
 		width:'85%',
--- src/Views/Contacto.js
+++ src/Views/Contacto.js
@@ -5,24 +5,59 @@ import ContactForm from '../Components/Form'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-function Contact () {
+function Contacto () {
 	return(
 		<div>
 
+			<div id="form" style={style.form}>
+			    <h1 style={style.titleTwo}>INFORMACIÓN DEL CLIENTE</h1>
+				
+				<ContactForm/>
+			</div>
+			
+			<div style={style.division}></div>
+
 			<Jumbotron fluid style={style.header}>
 			  <Container>
-			    <h1 style={style.title}>Contact Page</h1>
-			    <p>
-			      Get in touch and we’ll connect you with the business team nearest you.
-			    </p>
-			    <Button variant="light" href="#form">Contact</Button>
+			  	
+				    <h1 style={style.title}>INFORMACIÓN DE CONTACTO</h1>
+
+				
+				<Row>
+					<div style={style.division}></div>
+
+					
+					<Col style={style.info}>
+						<a target="_blank" href="https://www.google.com/maps/place/Plaza+Aventura+Business+Center/@9.0117052,-79.5338887,15z/data=!4m5!3m4!1s0x0:0xac39aba029fb1a56!8m2!3d9.0117052!4d-79.5338887"><img src="/Assets/address.png"/></a>
+						<br/>
+					    <a target="_blank" href="https://www.google.com/maps/place/Plaza+Aventura+Business+Center/@9.0117052,-79.5338887,15z/data=!4m5!3m4!1s0x0:0xac39aba029fb1a56!8m2!3d9.0117052!4d-79.5338887">Ave. Ricardo J. Alfaro, Plaza Aventura Mezanine No. 8 Panama City, Republica de Panamá</a>
+				    </Col>
+
+				    <div style={style.division}></div>
+
+					<Col style={style.info}>
+						<h3>Dirección General</h3>
+					    <a href="tel:+52 1 55 27377893">+52 1 55 27377893</a>
+					    <br/>
+					    <a href="mailto:atanacio.ocampo@kall-one.com">atanacio.ocampo@kall-one.com</a>
+				    </Col>
+				    
+
+				    
+				    <Col style={style.info}>
+						<h3>Asuntos Generales</h3>
+					    <a href="tel:+(507)-6532-9577">+(507)-6532-9577</a>
+					    <br/>
+					    <a href="mailto:contacto@kall-one. com">contacto@kall-one. com</a>
+				    </Col>
+				</Row>
 			  </Container>
 			</Jumbotron>
 
-			<div id="form" style={style.form}>
-				<ContactForm/>
-			</div>
+			<div style={style.division}></div>
 
 		</div>
 	)
@@ -37,19 +72,34 @@ const style = {
     backgroundSize: 'cover',
     padding: '25px',
     textAlign: 'center',
-    padding: '6em 0em 4em 0em',
+    padding: '2em 0em 4em 0em',
     fontSize: '18px',
 	letterSpacing: '0.095em',
-	backgroundColor: '#cccccc',
+	color: '#000',
+	backgroundColor: '#fff',
+  },
+  form: {
+  	padding: '0em 0em 0em 0em',
   },
   title: {
   	fontSize: '50px',
-  	margin: '2em 0em 3em 0em',
+  	margin: '0em 0em 1em 0em',
   },
-  form: {
-  	paddingTop: '3em'
+  titleTwo: {
+  	fontSize: '50px',
+  	textAlign: 'center',
+  	paddingTop: '2.5em'
+  },
+  division: {
+	margin: '0em 0em 1em 0em',
+	width: '100%',
+	height: '0.5em',
+	backgroundColor: '#00171f'
+  },
+  info: {
+  	margin: '2em',
   }
 
 }
 
-export default Contact;
+export default Contacto;
