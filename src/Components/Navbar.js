a3d08f403743f8cfe00792ccde3ce21d64ee8e80
jamalamar <jamal_ab22@hotmail.com>
2019-12-18 19:40:29 -0600

    landing page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,25 +4,35 @@ import {Link} from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 
 function NavBar (props) {
 
   return (
     <div className="App">
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
-          <Navbar.Brand style={style.brand} as={Link} to='/' >
-            <img src='/Assets/KallOne_Logo.png' style={style.logo}/>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto" style={style.navItems}>
-            <Nav.Link as={Link} to='/' style={style.link}>Inicio</Nav.Link>
-              <Nav.Link as={Link} to='/Productos' style={style.link}>Productos</Nav.Link>
+        <Navbar 
+          collapseOnSelect expand="lg" 
+          fixed="top"
+          bg='light'
+          variant='light'
+          >
+            <Navbar.Brand style={style.brand} as={Link} to='/' >
+              <img src='/Assets/KallOne_Logo.png' style={style.logo}/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto" style={style.navItems}>
+              <Nav.Link as={Link} to='/' style={style.link}>Inicio</Nav.Link>
               <Nav.Link as={Link} to="/QuienesSomos" style={style.link}>Quienes somos</Nav.Link>
+              <NavDropdown title="Productos">
+                <NavDropdown.Item as={Link} to='/Productos' >Aire Acondicionado</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to='/Productos' >Camiones</NavDropdown.Item>
+              </NavDropdown>
               <Nav.Link as={Link} to='/Contacto' style={style.link}>Contacto</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+              </Nav>
+            </Navbar.Collapse>
         </Navbar>
     </div>
   );
@@ -30,16 +40,16 @@ function NavBar (props) {
 
 
 const style = {
-
   logo: {
-    width: '160px'
+    width: '170px',
+    margin: '0em 2em 0em 2em',
+    padding: '0em 1em 0em 1em',
   },
   navItems: {
     letterSpacing: '0.1em',
     fontWeight: '702',
   },
   link: {
-    color: '#000000',
     margin: '0em 0em 0em 1em'
   }
 }
--- src/Views/Inicio.js
+++ src/Views/Inicio.js
@@ -2,49 +2,72 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 
 import Carousel from 'react-bootstrap/Carousel'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 
 function Home(){
 	return(
 		<div>
-
 			<Carousel style={style.carousel}>
-			  <Carousel.Item>
+			  <Carousel.Item style={style.carouselItem}>
 			    <img
 			      className="d-block w-100"
 			      src="Assets/transfer2/Slide_One.jpg"
 			      alt="First slide"
 			    />
-			    <Carousel.Caption>
-			      <h3>First slide label</h3>
-			      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+			    <Carousel.Caption style={style.caption}>
+			      <h2>Visión Global de Negocios</h2>
+			      <p>Para alcanzar cada objetivo planteado, cada colaborador tiene una visión conjunta de la empresa y los socios de negocios.</p>
 			    </Carousel.Caption>
 			  </Carousel.Item>
-			  <Carousel.Item>
+
+			  <Carousel.Item style={style.carouselItem}>
+			    <img
+			      className="d-block w-100"
+			      src="Assets/transfer2/Slide_Two.jpg"
+			      alt="Third slide"
+			    />
+
+			    <Carousel.Caption style={style.caption}>
+			      <h2>Calidad en los Productos</h2>
+			      <p>Nuestros proveedores se ocupan de cumplir con los más altos estándares de calidad y así asegurar la entrega de valor en cada producto.</p>
+			    </Carousel.Caption>
+			  </Carousel.Item>
+
+			  <Carousel.Item style={style.carouselItem}>
 			    <img
 			      className="d-block w-100"
 			      src="Assets/transfer2/Slide_One.jpg"
 			      alt="Third slide"
 			    />
 
-			    <Carousel.Caption>
-			      <h3>Second slide label</h3>
-			      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			    </Carousel.Caption>
-			  </Carousel.Item>
-			  <Carousel.Item>
-			    <img
-			      className="d-block w-100"
-			      src="Assets/transfer2/Slide_One.jpg"
-			      alt="Third slide"
-			    />
-
-			    <Carousel.Caption>
-			      <h3>Third slide label</h3>
-			      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+			    <Carousel.Caption style={style.caption}>
+			      <h2>Profesionalismo y Compromiso</h2>
+			      <p>Desarrollamos cada actividad de manera profesional con un total compromiso y responsabilidad hacia cada socio de negocios.</p>
 			    </Carousel.Caption>
 			  </Carousel.Item>
 			</Carousel>
+
+			<Container style={style.container}>
+			  <Row>
+			    <Col lg={true}>
+			    	<h1>Nuestra Misión</h1>
+					<p>Satisfacer las necesidades de transporte y climatización del mercado ofreciendo autobuses, 
+					equipos de climatización y refacciones automotrices de calidad, 
+					así como soluciones para su uso óptimo y mantenimiento.</p>
+				</Col>
+			    <Col lg={true}>
+			    	<h1>VISIÓN</h1>
+					<p>Convertirnos en los siguientes cinco años en una de las principales 
+					empresas del mercado centroamericano en la comercialización de autobuses, 
+					equipos de climatización, así como en refacciones automotrices 
+					y soluciones integrales para medios de transporte masivos.</p>
+			    </Col>
+			  </Row>
+
+			</Container>		
 
 		</div>
 	)
@@ -52,9 +75,18 @@ function Home(){
 
 const style = {
 	carousel: {
-		margin: 'auto',
-		padding: '5em 0em 0em 0em',
-		width: '100%',
+		padding: '5em 0em 0em 0em'
+	},
+	carouselItem: {
+		height: '450px',
+	},
+	caption: {
+		color: '#fff',
+		padding: '1.5em',
+		backgroundColor: 'rgba(0,0,0,0.6)'
+	},
+	container: {
+		padding: '2em'
 	}
 }
 
