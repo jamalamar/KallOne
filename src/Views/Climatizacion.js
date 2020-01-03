29f0f92b0f65b8ce766eb897f63bdf0beb782513
jamalamar <jamal_ab22@hotmail.com>
2020-01-03 13:28:22 -0600

    changes

--- src/Views/Climatizacion.js
+++ src/Views/Climatizacion.js
@@ -10,37 +10,33 @@ function Climatizacion() {
         <h1 style={style.title}>EQUIPOS DE CLIMATIZACIÓN</h1>
         <div style={style.divisionTwo}></div> 
 
-      <Card>
-        <Card.Img variant="top" src="Assets/transfer2/AC353.png" />
-          <Card.ImgOverlay>
-            <h1 style={style.cardTitle}>AC 353</h1>
-          </Card.ImgOverlay>
-        <Card.Body>
-          <Card.Text style={style.cardText}>
-            <p>El AC 353 ha demostrado una superioridad modélica
-               con la que ofrece a nivel mundial soluciones que logran el mas 
-               avanzado confort de los pasajeros. A menudo copiado, pero nunca 
-               igualado: el AC 353 es sinónimo de absoluto desempeño y confiabilidad. 
-               El sistema AC353 G4 aun va un paso mas allá y establece nuevos estándares 
-               de liderazgo en climatización de autobuses.</p>
+        <Card className="bg-light text-dark">
+          <Card.Img variant="top" src="Assets/transfer2/AC353.png" />
+            <Card.ImgOverlay>
+              <h1 style={style.cardTitle}>AC 353</h1>           
+            </Card.ImgOverlay>
+          <Card.Body>
+            <Card.Text style={style.cardText}>
+              <p>El AC 353 ha demostrado una superioridad modélica
+                 con la que ofrece a nivel mundial soluciones que logran el mas 
+                 avanzado confort de los pasajeros. A menudo copiado, pero nunca 
+                 igualado: el AC 353 es sinónimo de absoluto desempeño y confiabilidad. 
+                 El sistema AC353 G4 aun va un paso mas allá y establece nuevos estándares 
+                 de liderazgo en climatización de autobuses.</p>
 
-             <ul style={style.cardList}>
-               <li>Completo espectro de capacidades para aplicaciones en zonas climáticas templadas y países cálidos.</li>
-               <br/>
-               <li>Diseño modular especialmente compacto gracias a la avanzada tecnología de intercambiador de calor MCHX.</li>
-               <br/>
-               <li>Sencillo montaje que reduce el trabajo de instalación del fabricante de autobuses.</li>
-               <br/>
-               <li>Cantidad de refrigerante reducida para un impacto medioambiental menor y menor costo de mantenimiento.</li>
-               <br/>
-               <li>Superficie de montaje estandarizada en todas las variantes del modelo.</li>
-               <br/>
-               <li>Reducidos costos de ciclo de vida y mayor facilidad de mantenimiento.</li>
-             </ul>
+               <ul style={style.cardList}>
+                 <li style={style.listItem}>Completo espectro de capacidades para aplicaciones en zonas climáticas templadas y países cálidos.</li>
+                 <li style={style.listItem}>Diseño modular especialmente compacto gracias a la avanzada tecnología de intercambiador de calor MCHX.</li>
+                 <li style={style.listItem}>Sencillo montaje que reduce el trabajo de instalación del fabricante de autobuses.</li>
+                 <li style={style.listItem}>Cantidad de refrigerante reducida para un impacto medioambiental menor y menor costo de mantenimiento.</li>
+                 <li style={style.listItem}>Superficie de montaje estandarizada en todas las variantes del modelo.</li>
+                 <li style={style.listItem}>Reducidos costos de ciclo de vida y mayor facilidad de mantenimiento.</li>
+               </ul>
 
-          </Card.Text>
-        </Card.Body>
-      </Card>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
 
 
     </Container>
@@ -50,12 +46,18 @@ const style = {
   
   title: {
       textAlign: 'center',
-      padding: '2.5em 0em 0em 0em',
-      fontSize: '50px'
+      padding: '3em 0em 0em 0em',
+      fontSize: '40px'
     },
     container: {
       margin: 'auto',
-      padding: '0em 0em 2em 0em'
+      padding: '0em 2em 2em 2em',
+    },
+    card: {
+      display: 'block',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      width: '85%',
     },
     cardTitle: {
       textAlign: 'justify',
@@ -63,12 +65,18 @@ const style = {
       fontWeight: '900',
     },
     cardText: {
+      fontSize: '19px',
       textAlign: 'justify',
-      letterSpacing: '0.1em'
+      letterSpacing: '0.1em',
+      padding: '1em'
     },
     cardList: {
+      fontSize: '14px',
       letterSpacing: '0.1em',
       fontWeight: '702',
+    },
+    listItem: {
+      padding: '1em 0em 0em 0em'
     },
     divisionTwo: {
       margin: '2em 0em 2em 0em',
